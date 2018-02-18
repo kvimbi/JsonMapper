d5a277675d8615f52a2ba431c352338509c55471
kvimbi <tomas@michalek.it>
2018-02-19 00:41:24 +0100

    Added tests

---
+++ TypeMapping.test.js
@@ -0,0 +1,142 @@
+import mapJson, {
+  Complex,
+  ComplexArray,
+  Float,
+  Int,
+  UnixTimestampStringDateMapper,
+} from './TypeMapping';
+import BigNumber from 'bignumber.js';
+
+const TestType = {
+  a: String,
+  b: Int,
+  c: Date,
+  d: BigNumber,
+  e: Float,
+  f: {
+    mapper: UnixTimestampStringDateMapper,
+  }, // unix time stamp
+  g: {
+    defaultValue: 'This is a default string value',
+  },
+  h: {
+    exclude: true,
+  },
+};
+
+describe('Type mapping testing', () => {
+  it('maps string correctly ignoring undocumented', () => {
+    const result = mapJson({ a: 65 }, TestType, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: '65' });
+  });
+
+  it('maps int correctly ignoring undocumented', () => {
+    const result = mapJson({ b: '65.5' }, TestType, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ b: 65 });
+  });
+
+  it('maps string date correctly ignoring undocumented', () => {
+    const result = mapJson({ c: '2018-05-14T03:25:20+0100' }, TestType, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ c: new Date('2018-05-14T02:25:20.000Z') });
+  });
+
+  it('maps string "true" boolean correctly ignoring undocumented', () => {
+    const result = mapJson({ a: 'true' }, { a: Boolean }, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: true });
+  });
+
+  it('maps string "false" boolean correctly ignoring undocumented', () => {
+    const result = mapJson({ a: 'false' }, { a: Boolean }, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: false });
+  });
+
+  it('maps string "0" boolean correctly ignoring undocumented', () => {
+    const result = mapJson({ a: '0' }, { a: Boolean }, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: false });
+  });
+
+  it('maps number "0" boolean correctly ignoring undocumented', () => {
+    const result = mapJson({ a: 0 }, { a: Boolean }, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: false });
+  });
+
+  it('maps number "1" boolean correctly ignoring undocumented', () => {
+    const result = mapJson({ a: 1 }, { a: Boolean }, {});
+    expect(typeof result === 'object').toBe(true);
+    expect(result).toEqual({ a: true });
+  });
+
+  it('maps string big number correctly ignoring undocumented', () => {
+    const result = mapJson({ d: '56478356348.34543543543534' }, TestType, {});
+    expect(typeof result.d).toBe('object');
+    expect(
+      result.d.comparedTo(new BigNumber('56478356348.34543543543534'))
+    ).toBe(0);
+  });
+
+  it('maps float number correctly ignoring undocumented', () => {
+    const result = mapJson({ e: '56478356348.34543543543534' }, TestType, {});
+    expect(typeof result.e).toBe('number');
+    expect(result.e).toBe(56478356348.34543543543534);
+  });
+
+  it('maps float number correctly changing name ignoring undocumented', () => {
+    const result = mapJson(
+      { e: '56478356348.34543543543534' },
+      { e: { toName: 'voldemort', type: Float } },
+      {}
+    );
+    expect(typeof result.voldemort).toBe('number');
+    expect(result).toEqual({ voldemort: 56478356348.34543543543534 });
+  });
+
+  it('ignoring undocumented attribute', () => {
+    const result = mapJson({ xyz: '56478356348.34543543543534' }, TestType, {});
+    expect(result).toEqual({});
+  });
+
+  it('including undocumented attribute', () => {
+    const result = mapJson({ xyz: '56478356348.34543543543534' }, TestType, {
+      includeUndescribed: true,
+    });
+    expect(result).toEqual({ xyz: '56478356348.34543543543534' });
+  });
+
+  it('fills missing values with default null', () => {
+    const result = mapJson({}, TestType, { fillMissing: true });
+    expect(result).toEqual({
+      a: null,
+      b: null,
+      c: null,
+      d: null,
+      e: null,
+      f: null,
+      g: 'This is a default string value',
+    });
+  });
+
+  it('maps complex object correctly', () => {
+    const result = mapJson(
+      { a: { b: '1234' } },
+      { a: new Complex({ b: Int }) }
+    );
+    expect(result).toEqual({ a: { b: 1234 } });
+  });
+
+  it('maps complex array object correctly', () => {
+    const result = mapJson(
+      { a: ['1', '2', '3'] },
+      {
+        a: new ComplexArray(Int),
+      }
+    );
+    expect(result).toEqual({ a: [1, 2, 3] });
+  });
+});
